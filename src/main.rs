4ae79bb651bdcab2a73bda690fe1d30e0a749bfe
John Grace <jmgrace@andrew.cmu.edu>
2020-04-25 10:53:25 -0400

    none

--- src/main.rs
+++ src/main.rs
@@ -1,146 +1,135 @@
-extern crate num_bigint;
-extern crate num_traits;
-extern crate modinverse;
+type Nogood = Vec<u8>;
 
-use std::mem::replace;
-
-use num_bigint::{BigUInt, ToBigInt, BigInt, Sign};
-use num_traits::{Zero, One};
-
-use modinverse::modinverse;
-
-struct Primitives {
-    width : usize,
-    m : usize,
-    n : BigInt,
-    r : BigInt,
-    inv : BigInt,
-    exp : BigInt,
-    dexp : BigInt
+struct AgentState {
+    id: u8,
+    pos: Vec<u8>,
+    no_goods: Vec<Nogood>,
+    new_no_goods: Vec<Nogood>,
+    oks: Vec<(u8,u8)>,
+}
+    
+//checks for consistent queen placement
+fn consistent(ar: u8, ac: u8, br: u8, bc: u8) {
+    // -1 signifies that agent ar is not assigned
+    if ac == -1 {
+        return true;
+    }
+    if ac == bc {
+        return false;
+    }
+    if ar - ac == br - bc {
+        return false;
+    }
+    if ar - ac == bc - br {
+        return false;
+    }
+    return true;
 }
 
-fn make_primitives(width : usize) -> Primitives {
-    let exp = ToBigInt::to_bigint(&65537);
 
-    // when picking p and q ensure that exp is coprime to (p - 1) and (q - 1)
-    let p = BigInt::parse_bytes(b"148715450009406215544851404632970187055827987065657638482806886212433040335306709375657187526428825846441364529015687294103246278898356371822895605129223904973676488684092873351933448486276982448871026358660306862802318591414961154419903429418827756491839789904016335301529951459787672947843136480252146828697", 10);
-    let q = BigInt::parse_bytes(b"50729716869762510472957508551392924763015740203992845666542245466801494776486627862032821248558918608127925860412000975488095246401743393430586416646520115373885609513042364141025003284261617251855726831653550557210202048890913945850247146994086207936675898070735266171909273984651769282447133701615228003941", 10);
+fn eq_part_ass(pa1: Vec<u8>, pa2: Vec<u8>) -> bool {
+    let small_length = min(pa1.len(), pa2.len());
+    for i in 0..small_length {
+        
+        // -1 means its unassigned
+        if pa1[i] == -1 || pa2[i] == -1 {
+            continue;
+        }
+        if pa1[i] != pa2[i] {
+            return false;
+        }
+    }
+    return true;
+}
 
-    let n = p * q;
 
-    let dexp : BigInt = match modinverse(exp, (p - 1)*(q - 1)) {
-        Some v0 => v0,
-        None => panic!("couldnt get dexp"),
+fn update_pos(agent: u8, states: Vec<AgentState>) -> bool {
+    let max_pos = 3;
+
+    // must check for it being too big here because when we found that a Nogood
+    // prevented an otherwise acceptable state, we increment a position,
+    // and it could possibly go out of bounds. If we do, we want to send
+    // a Nogood to the predecessor. 
+    if states[agent].pos[agent] < max_pos {
+        states[agent].pos[agent] = 0;
+        return false;
+    }
+ 
+    // select position that is consistent with predecessors
+    for i in 0..agent {
+        while false == consistent(i, states[agent].pos[i],
+                                agent, states[agent].pos[agent]) {
+            states[agent].pos[agent] = states[agent].pos[agent] + 1;
+            if states[agent].pos[agent] > max_pos {
+                states[agent].pos[agent] = 0;
+                return false;
+            }
+        }
+
+    }
+    true
+}
+
+
+fn run_agent(agent: u8, states: Vec<AgentState>) {
+    // first update the local view from the ok messages queue.
+    // in this sequential version, they're already updated.
+    // then look to see if the current agent has a consistent assignment.
+    // if not, send a Nogood. If so, check to make sure that it's not ruled
+    // out by a Nogood.
+    if false == update_pos(agent, states) {
+        // must send no good to lowest affected member
+        // I think that in the queens problem it will always be the immediate
+        // predecessor
+        // also assume that the lowest affected member changes its position
+        // we don't know what it gets changed to, but remove it as a conflict
+        let imm_pred = agent - 1;
+
+        //send Nogood
+        let nogood = states[agent].pos[0..agent].to_vec();
+        states[imm_pred].no_goods.push(nogood);
+
+        // erase agent's belief about its predecessor's position
+        states[agent].pos[imm_pred] = -1;
+        if false == update_pos(agent, states) {
+            panic!("failed to get working position");
+        }
+        // if you send a Nogood, you don't send an ok message
+        return;
+    }
+    // Now that a consistent assignment has been found, check to see if it's
+    // ruled out by a Nogood.
+
+    while(states[agent].no_goods.len() > 0) {
+        let no_good = states[agent].no_goods.pop();
+        if eq_part_ass(no_good, states[agent].pos) {
+            states[agent].pos[agent] = states[agent].pos[agent] + 1;
+            run_agent(agent, states);
+            return;
+        }
+        
+
     }
 
-    // width must be a power of 2
-    // n will be odd.
-    // r is 2^(2*width)
-    // n will be less than r.
+}
 
-    let prims = Primitives {
-        width: width,
-        m : width * 2,
-        n : n,
-        r : make_r(m),
-        inv : make_inv(m, n),
-        exp : exp,
-        dexp : dexp,
+
+fn make_agents(num_agents: u8) -> Vec<AgentState> {
+    let mut agents: Vec<AgentState> = vec![];
+    for i in 0..num_agents {
+        let agent = AgentState {
+            id: i,
+            pos: vec![0u8; num_agents],
+            no_goods: vec![],
+            new_no_goods: vec![],
+            oks: vec![],
+        }
+        agents.push(agent);
     }
-
-
+    agents
 }
-
-
-
-fn make_r(m : usize) -> BigInt {
-    let power = m / 32;
-
-    // to make r, we need to represent it as a base 2^32 number to pass
-    // it to the BigInt constructor
-    let digits = Vec::<u32>::new();
-    for _ in 0..power {
-        digits.push(0);
-    }
-    let leftoverexp = m - 32 * power;
-    digits.push(2^leftoverexp);
-    BigInt::new(Sign::Plus, digits);
-}
-
-fn make_inv(m : usize, n : BigInt) -> BigInt{
-    let mut inv : BigInt = 1;
-    let e = m.trailing_zeros() + 1;
-    for _ in 1..e {
-        inv = inv * (2 - n * inv);
-    }
-    inv;
-}
-
-
-// compute x * r^(-1) mod n
-// r = 2^m
-// The parameters satisify r * r^(-1) + n * inv = 1
-
-fn reduce(x : BigInt) {
-    let xmodr : BigInt = x && (r - 1);
-    let inv_modr : BigInt = inv && (r - 1);
-    let q = xmodr * inv_modr;
-    let a = (x - q * n) >> m;
-    if a < 0 {
-        a = a + n;
-    }
-    a
-}
-
-// assumes the multiplication is being done modulo n and that the r value
-// to put the numbers in montgomery space is r.
-fn mont_mult(x : BigInt, y : BigInt) -> BigInt{
-    reduce(x * y);
-}
-
-// mess must be smaller than n
-fn mod_exp_by_squaring(mess : BigInt, exp : BigInt, n : BigInt) -> BigInt {
-    let mut curr_exp = 1;
-    let mut res = mess;
-
-    // TODO maybe this loop could be faster by calculating log first and then
-    // doing additions instead of doing some multiplications
-    while (curr_exp  * 2 <= exp) {
-        res = mont_mult(res, res);
-        curr_exp = curr_exp * 2;
-    }
-    res = res * mod_exp_by_squaring(mess, exp - curr_exp, n);
-    res
-}
-
-
-fn encrypt(mess : BigInt, exp : BigInt, n : BigInt) -> BigInt {
-    mod_exp_by_squaring(mess, exp, n);
-}
-
-fn decrypt(ciphertext : BigInt, dexp : BigInt, n : BigInt) -> BigInt {
-    mod_exp_by_squaring(ciphertext, dexp, n);
-}
-
-
-// Calculate large fibonacci numbers.
-fn fib(n: usize) -> BigUint {
-    let mut f0: BigUint = Zero::zero();
-    let mut f1: BigUint = One::one();
-    for _ in 0..n {
-        let f2 = f0 + &f1;
-        // This is a low cost way of swapping f0 with f1 and f1 with f2.
-        f0 = replace(&mut f1, f2);
-    }
-    f0
-}
-
 
 fn main() {
-    
-
-
-
-
-    println!("fib(1000) = {}", fib(1000));
+    let num_agents = 4;
+    let agents = make_agents(num_agents);
 }
